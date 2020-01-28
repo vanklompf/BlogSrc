cbc9223a460f073b5a865da8e08bd51f6d124ba7
vanKlompf <fkorta@gmail.com>
2020-01-28 23:06:09 +0100

    More changes to AvxThrottle

--- AvxThrottle/main.cpp
+++ AvxThrottle/main.cpp
@@ -1,76 +1,82 @@
 #include <cstdio>
 #include <chrono>
 #include <stdint.h>
-#include <random>
 #include <algorithm>
 #include <string.h>
+
+#include <stdlib.h>
 
 static constexpr int BUFFER_SIZE = 1024 * 1024 * 2;
 static constexpr int STEP = 512;
 static constexpr int REPEAT = 10000;
 
-std::mt19937 mersenne_engine(42);
-std::uniform_int_distribution<uint8_t> dist {1, 250};
-auto gen = [](){return dist(mersenne_engine);};
+const int m = 0xFFFFFFFF;
+const int a = 1664525;
+const int c = 1013904223;
+static int seed = 123456789;
+
+int rand() {
+  seed = (a * seed + c) % m;
+  return seed;
+}
+auto gen = [](){return rand() % 250;};
 static int no_opt = 0;
 
-static void perf_memcpy(uint8_t* buffer) {
+void perf_memcpy(uint8_t* buffer) {
     for(int r=0; r<REPEAT; r++) 
     {
         for(int i=0; i<BUFFER_SIZE; i+=STEP)
         {
             auto pos = std::find(buffer + i, buffer + i + STEP, 253);
             auto dist = std::distance(buffer, pos);
-            //printf("dist: %ld\n", dist);
             no_opt += dist;
             memcpy(buffer + i, buffer + i + STEP, STEP);
         }
     }
 }
 
-static void perf_copy(uint8_t* buffer) {
+void perf_copy(uint8_t* buffer) {
     for(int r=0; r<REPEAT; r++) 
     {
         for(int i=0; i<BUFFER_SIZE; i+=STEP)
         {
             auto pos = std::find(buffer + i, buffer + i + STEP, 253);
             auto dist = std::distance(buffer, pos);
-            //printf("dist: %ld\n", dist);
             no_opt += dist;
-            //std::copy(buffer + i + STEP, buffer + i + STEP + STEP, buffer + i);
             __builtin_memcpy(buffer + i, buffer + i + STEP, STEP);
         }
     }
 }
 
 
-int main() {
+int main(int argc, char** argv) {
 	printf("Impact of Avx2 and Avx512 on CPU clock and performance\n");
 	printf("For details check: https://extensa.tech\n");
     uint8_t* buffer = new uint8_t[BUFFER_SIZE + STEP];
-    std::generate(buffer, buffer + BUFFER_SIZE + STEP, gen);
-    /*for (int i=0;i<BUFFER_SIZE;i++) 
-    {
-        printf("buf[]: %d\n", (int)buffer[i]);
-    }*/
+    srandom(42);
     
+    //std::generate(buffer, buffer + BUFFER_SIZE + STEP, gen);
+
     //warm up
     for(int i=0; i<BUFFER_SIZE; i+=STEP)
-        memcpy(buffer + i, buffer + i + STEP, STEP);
+        __builtin_memcpy(buffer + i, buffer + i + STEP, STEP);
 
+    char arg = 'c';
+    if (argc > 1) 
+        arg = argv[1][0];
+        
     auto start = std::chrono::high_resolution_clock::now();
-    perf_copy(buffer);
+    if (arg == 'c') {
+        perf_copy(buffer);
+        printf("Time copy: ");
+    } else {
+        perf_memcpy(buffer);
+        printf("Time memcpy: ");
+    }
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    printf("Time copy: %ld ms\n", duration);
-    
-    start = std::chrono::high_resolution_clock::now();
-    perf_memcpy(buffer);
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    printf("Time memcpy: %ld ms\n", duration);
-    
-    printf("AntiOpt: %d\n", no_opt);    
+    printf("%ld ms\n", duration);
+    printf("AntiOpt: %d\n", no_opt);
     
     delete[] buffer;
 }
