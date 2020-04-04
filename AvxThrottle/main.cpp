d8aabd84c60f5f7b8bde09ff5cfb3f3ef6e0d133
Franciszek Korta <franciszek.korta@corvil.com>
2020-04-04 15:20:56 +0200

    Working example of AVX512 issue

--- AvxThrottle/main.cpp
+++ AvxThrottle/main.cpp
@@ -3,76 +3,108 @@
 #include <stdint.h>
 #include <algorithm>
 #include <string.h>
+#include <numeric>
 
 #include <stdlib.h>
 
 static constexpr int BUFFER_SIZE = 1024 * 1024 * 2;
-static constexpr int STEP = 512;
-static constexpr int REPEAT = 10000;
+static constexpr int STEP = 2048;
+static constexpr int REPEAT = 2000;
 
-const int m = 0xFFFFFFFF;
-const int a = 1664525;
-const int c = 1013904223;
-static int seed = 123456789;
+struct Point { float x; float y; float z; } __attribute__ ((aligned (16)));
+typedef Point NumType;
 
-int rand() {
-  seed = (a * seed + c) % m;
-  return seed;
-}
-auto gen = [](){return rand() % 250;};
 static int no_opt = 0;
 
-void perf_memcpy(uint8_t* buffer) {
+inline float sumsq( struct Point *ptvec, int n) {
+     float t_sum = 0;
+     for (int i=0; i<n; i++) {
+        t_sum += ptvec[i].x * ptvec[i].x;
+        t_sum += ptvec[i].y * ptvec[i].y;
+        t_sum += ptvec[i].z * ptvec[i].z;
+     }
+     return t_sum;
+}
+
+void float_math_default(NumType* buffer) {
+    printf("Running: default optimization\n");
     for(int r=0; r<REPEAT; r++) 
     {
         for(int i=0; i<BUFFER_SIZE; i+=STEP)
         {
-            auto pos = std::find(buffer + i, buffer + i + STEP, 253);
-            auto dist = std::distance(buffer, pos);
-            no_opt += dist;
-            memcpy(buffer + i, buffer + i + STEP, STEP);
+            no_opt += sumsq(buffer + i, STEP);
         }
     }
 }
 
-void perf_copy(uint8_t* buffer) {
+
+__attribute__ ((__target__ ("avx")))
+void float_math_avx(NumType* buffer) {
+    printf("Running: avx optimized\n");
     for(int r=0; r<REPEAT; r++) 
     {
         for(int i=0; i<BUFFER_SIZE; i+=STEP)
         {
-            auto pos = std::find(buffer + i, buffer + i + STEP, 253);
-            auto dist = std::distance(buffer, pos);
-            no_opt += dist;
-            __builtin_memcpy(buffer + i, buffer + i + STEP, STEP);
+            no_opt += sumsq(buffer + i, STEP);
         }
     }
 }
 
+__attribute__ ((__target__ ("avx2")))
+void float_math_avx2(NumType* buffer) {
+    printf("Running: avx2 optimized\n");
+    for(int r=0; r<REPEAT; r++) 
+    {
+        for(int i=0; i<BUFFER_SIZE; i+=STEP)
+        {
+            no_opt += sumsq(buffer + i, STEP);
+        }
+    }
+}
+
+__attribute__ ((__target__ ("avx512f,avx512cd,avx512vl,avx512bw,avx512dq")))
+void float_math_avx512(NumType* buffer) {
+    printf("Running: avx512 optimized\n");
+    for(int r=0; r<REPEAT; r++) 
+    {
+        for(int i=0; i<BUFFER_SIZE; i+=STEP)
+        {
+            no_opt += sumsq(buffer + i, STEP);
+        }
+    }
+}
 
 int main(int argc, char** argv) {
-	printf("Impact of Avx2 and Avx512 on CPU clock and performance\n");
+	printf("Impact of Avx2 and Avx512 on CPU clock and performance. Example is using float numbers crunching as CPU load.\n");
 	printf("For details check: https://extensa.tech\n");
-    uint8_t* buffer = new uint8_t[BUFFER_SIZE + STEP];
-    srandom(42);
-    
-    //std::generate(buffer, buffer + BUFFER_SIZE + STEP, gen);
+    auto buffer = new NumType[BUFFER_SIZE + STEP];
 
     //warm up
     for(int i=0; i<BUFFER_SIZE; i+=STEP)
-        __builtin_memcpy(buffer + i, buffer + i + STEP, STEP);
+        __builtin_memcpy(buffer + i, buffer + i + STEP, STEP * sizeof(NumType));
 
-    char arg = 'c';
+    char arg = 'x';
     if (argc > 1) 
         arg = argv[1][0];
-        
-    auto start = std::chrono::high_resolution_clock::now();
-    if (arg == 'c') {
-        perf_copy(buffer);
-        printf("Time copy: ");
-    } else {
-        perf_memcpy(buffer);
-        printf("Time memcpy: ");
+    
+    auto fn = float_math_default;
+    switch (arg) {
+        case '0': fn = float_math_default; break;
+        case '1': fn = float_math_avx; break;
+        case '2': fn = float_math_avx2; break;
+        case '3': fn = float_math_avx512; break;
+        default:
+            printf("Parameter required:\n");
+            printf("0 - default optimization\n");
+            printf("1 - avx optimized\n");
+            printf("2 - avx2 optimized\n");
+            printf("3 - avx512 optimized\n");
+            exit(1);
+        break;
     }
+    
+    auto start = std::chrono::high_resolution_clock::now();
+    fn(buffer);
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     printf("%ld ms\n", duration);
