997698be6b9752176e1bae5149d5d5b3e2ddd7f5
vanklompf <fkorta@gmail.com>
2020-10-04 21:45:57 +0200

    Initial version of new FastMerging benchmark

---
+++ FastMerging/main.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+
+int main() {
+	printf("Synthetic benchmark measuring various strategies for merging vectors of sorted integers \n");
+	printf("For details check: https://extensa.tech\n");
+}
